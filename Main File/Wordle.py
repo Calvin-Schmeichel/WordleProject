03bb0bd8a1d785a902b0b343e27f4624df1d4768
CalvinSchmeichel <calvinschmeichel@yahoo.com>
2022-12-02 16:45:09 -0600

    WordleV3

--- Main File/Wordle.py
+++ Main File/Wordle.py
@@ -4,14 +4,22 @@ class bcolors:
     ENDC = '\033[0m'
 
 
-def  WordCheck(guess,word,GuessHistory, index):
+def  WordCheck(guess,word,GuessHistory, index, WordHistory):
+    GreenCount = -1
     for n in range(0,5):
+        #print("guess find", word.find(guess[n]))
+        #print("word find", word.find(word[n]))
+        
         if word[n] == guess[n]:
             GuessHistory[index] += str(f"{bcolors.GREEN}{guess[n]}{bcolors.ENDC}")
-        elif word.find(guess[n]) != -1:
+            GreenCount += 1
+        elif word.find(guess[n]) != -1 and word.find(guess[n]) >= word.find(word[n]):
             GuessHistory[index] += str(f"{bcolors.YELLOW}{guess[n]}{bcolors.ENDC}")
         else:
             GuessHistory[index] += guess[n]
+    
+    if guess == word:
+        GameEndScreen(GuessHistory, index, word, guess, WordHistory)
 
 
 def WordFromUser(WordHistory):
@@ -26,13 +34,37 @@ def WordFromUser(WordHistory):
     return guess
 
 
-def DrawGameBoard(GuessHistory, AttemptCount, word):
+def DrawGameBoard(GuessHistory, AttemptCount):
 
     for n in range(0,AttemptCount):
         print(GuessHistory[n])
 
     for n in range(0, 6 - AttemptCount):
         print("▯▯▯▯▯")
+
+def GameEndScreen(GuessHistory, AttemptCount, word, guess, WordHistory):
+    if AttemptCount != 6:
+        print(f"{bcolors.GREEN}You guessed the word! in {AttemptCount+1} attempt(s)! {bcolors.ENDC}")
+    else:
+        print(f"{bcolors.YELLOW}You did not guess the word. The word was: {word}{bcolors.ENDC}")
+    
+    for k in WordHistory:
+        guess = k
+        for n in range(0,5):
+            if word[n] == guess[n]:
+                print(f"{bcolors.GREEN}▯{bcolors.ENDC}",end ="")
+            elif word.find(guess[n]) != -1:
+                print(f"{bcolors.YELLOW}▯{bcolors.ENDC}",end ="")
+            else:
+                print("▯",end ="")
+        
+        print()
+
+
+    for n in range(0, 5 - AttemptCount):
+        print("▯▯▯▯▯")
+
+    exit()
 
 
 def main():
@@ -48,8 +80,10 @@ def main():
     for AttemptCount in range(1,7):
         guess = WordFromUser(WordHistory)
         print()
-        WordCheck(guess,word,GuessHistory, AttemptCount-1)
-        DrawGameBoard(GuessHistory, AttemptCount, word)
+        WordCheck(guess,word,GuessHistory, AttemptCount-1, WordHistory)
+        DrawGameBoard(GuessHistory, AttemptCount)
+    
+    GameEndScreen(GuessHistory, AttemptCount, word, guess, WordHistory)
 
 
 #This is so we can import the functions in this program in other code without having to run the main function
