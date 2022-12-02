6f7b92c64d4ca26aca51f3f2a5735544e998ed89
CalvinSchmeichel <calvinschmeichel@yahoo.com>
2022-12-02 16:46:48 -0600

    WordleV5

--- Main File/Wordle.py
+++ Main File/Wordle.py
@@ -4,34 +4,27 @@ class bcolors:
     ENDC = '\033[0m'
 
 
-def  WordCheck(guess,word,GuessHistory, index, GameSquares):
-
-    for k in range(0,5):
-        ColorCount = 0    
-        for n in range(0,5):
-            #print(word.count(guess[n]))
-            if word[n] == guess[n]:
-                GuessHistory[index][n] = str(f"{bcolors.GREEN}{GuessHistory[index][n]}{bcolors.ENDC}")
-                GameSquares[index][n] = str(f"{bcolors.GREEN}▯{bcolors.ENDC}")
-                ColorCount += 1
-        for n in range(0,5):
-            if (guess[n] in word) == True and ("32m" in GuessHistory[index][n]) == False and (word.count(word[k])) > ColorCount:
-                GuessHistory[index][n] = str(f"{bcolors.YELLOW}{GuessHistory[index][n]}{bcolors.ENDC}")
-                GameSquares[index][n] = str(f"{bcolors.YELLOW}▯{bcolors.ENDC}")
-                ColorCount += 1
-
-    if guess == word:
-        GameEndScreen(index+1, word, GameSquares)
-        
-
-            
-        
+def WordCheck(WordLetterslist, guess, word, WordLetters, GuessHistory, index):
     
+    for k in range(0, len(WordLetterslist)):
 
- 
+        ColorLimit = len(WordLetterslist[k])
+        ColorCount = 0
 
+        for n in range(0, len(WordLetterslist[k])):
+            if word[WordLetterslist[k][n]] == guess[WordLetterslist[k][n]]:
+                GuessHistory[index][WordLetterslist[k][n]] = str(f"{bcolors.GREEN}{GuessHistory[index][WordLetterslist[k][n]]}{bcolors.ENDC}")
+                ColorCount += 1
 
-
+        for n in range(0, 5):
+            if (WordLetters[k] in guess) == True and (ColorLimit > ColorCount) and guess[n] == WordLetters[k] and (("32mp" in GuessHistory[index][n]) == False):
+                GuessHistory[index][n] = str(f"{bcolors.YELLOW}{GuessHistory[index][n]}{bcolors.ENDC}")
+                ColorCount += 1
+    
+    if guess == word:
+        DrawGameBoard(GuessHistory, index+1)
+        GameEndScreen(index+1, word)
+        
 
 def WordFromUser(WordHistory, GuessHistory):
     guess = input(":")
@@ -42,7 +35,6 @@ def WordFromUser(WordHistory, GuessHistory):
 
     WordHistory.append(guess)
     GuessHistory.append(list(guess))
-    #print(GuessHistory)
     return guess
 
 
@@ -57,20 +49,12 @@ def DrawGameBoard(GuessHistory, AttemptCount):
         print("▯▯▯▯▯")
 
 
-def GameEndScreen(AttemptCount, word, GameSquares):
-    if AttemptCount != 6:
-        print(f"{bcolors.GREEN}You guessed the word! in {AttemptCount+1} attempt(s)! {bcolors.ENDC}")
+def GameEndScreen(AttemptCount, word):
+    if AttemptCount < 7:
+        print(f"{bcolors.GREEN}You guessed the word! in {AttemptCount} attempt(s)! {bcolors.ENDC}")
     else:
         print(f"{bcolors.YELLOW}You did not guess the word. The word was: {word}{bcolors.ENDC}")
         
-        print()
-
-    
-
-    for k in range (0,AttemptCount):
-        for n in range(0,5):
-            print(GameSquares[k][n], end = "")
-            
         print()
 
     exit()
@@ -85,15 +69,21 @@ def main():
 
     print(f"{bcolors.GREEN}Welcome to Wordle enter a word{bcolors.ENDC}")
 
-    word = "apple"
+    word = "bingo"
 
+    WordLetters = (''.join(sorted(set(word))))
+    WordLetterslist = []
+   
+    for n in range(0,len(WordLetters)):
+        WordLetterslist.append([i for i, ltr in enumerate(word) if ltr == WordLetters[n]])
+    
     for AttemptCount in range(1,7):
         guess = WordFromUser(WordHistory,GuessHistory)
         print()
-        WordCheck(guess,word,GuessHistory, AttemptCount-1, GameSquares)
+        WordCheck(WordLetterslist, guess, word, WordLetters, GuessHistory, AttemptCount-1)
         DrawGameBoard(GuessHistory, AttemptCount)
     
-    GameEndScreen(AttemptCount, word, GameSquares)
+    GameEndScreen(6, word)
 
 
 #This is so we can import the functions in this program in other code without having to run the main function
